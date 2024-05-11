e153bb128257b7e187c0714f5206e1686f47ba0b
Mayur Bhoyar <mayurbhoyar4455@gmail.com>
2024-05-11 10:28:11 +0530

    datadisplayissuefixed

--- server/routes/kpi.js
+++ server/routes/kpi.js
@@ -1,11 +1,11 @@
 import express from "express";
 import KPI from "../models/KPI.js";
-
+import { kpis } from "../data/data.js";
 const router = express.Router();
 
 router.get("/kpis", async (req, res) => {
   try {
-    const kpis = await KPI.find();
+    // const kpis = await KPI.find();
     res.status(200).json(kpis);
   } catch (error) {
     res.status(404).json({ message: error.message });
